d3978045cbe299b41d0d3761dcc1488b55aa46e7
PIL17003689 <pil17003689@btc.ac.uk>
2024-04-24 09:22:16 +0100

    Changing from PHP to HTML to support flask format

--- Practical/templates/forecast.php
+++
@@ -1,8 +0,0 @@
-<?php $title = 'Weather Forecasting';
-require __DIR__ . "/inc/header.php"; ?>
-
-<div class="centered-body" style="margin-top:10vh;">
-    <h1 class="titleText"> This is the Weather Forecasting Page</h1>
-</div>
-
-<?php require __DIR__ . "/inc/footer.php"; ?> 
